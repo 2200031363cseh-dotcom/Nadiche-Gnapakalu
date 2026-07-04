b0f6bf8f5a4d1aeae032dbfeab0192becad809ce
2200031363cseh-dotcom <2200031363cseh@gmail.com>
2026-07-05 02:18:27 +0530

    do it

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -91,7 +91,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "/book/book_front.png", type: "image/png" },
     ],
   }),
   shellComponent: RootShell,
